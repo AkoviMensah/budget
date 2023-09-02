978a5181ab2424737a3526aa65a465d9290b11b0
bobovi <abmensah@wisc.edu>
2023-09-02 12:59:16 -0500

    added BudgetCard component

--- src/App.js
+++ src/App.js
@@ -3,8 +3,17 @@ import { Button, Stack } from "react-bootstrap";
 
 function App() {
   return (
-    <Container>
-      Budget App
+    <Container className="my-4">
+      <Stack direction="horizontal" gap="2" className="mb-4">
+        <h1 className="me-auto">Budget</h1>
+        <Button variant="primary">Add Budget</Button>
+        <Button variant="outline-primary">Add Expense</Button>
+      </Stack>
+
+      <div style={{display: "grid", gridTemplateColumns: "repeat(auto-fill,minmax(300px, 1fr))",
+    gap: "1rem", alignItems: "flex-start"}}>
+
+      </div>
     </Container>
   );
 }
